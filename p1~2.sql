4e3e4f5413af70c06224dab9b22e93576739bd28
Ying <xueying19981206@gmail.com>
2019-10-05 17:50:04 +0200

    modify

--- p1~2.sql
+++ p1~2.sql
@@ -1,53 +1,6 @@
---select * from user_tables;
-DROP TABLE test1;
-CREATE TABLE test1(col1 INTEGER PRIMARY KEY, col2 VARCHAR2(20));
-
-
-
-select * from user_tables where table_name like 'TE%';
-insert into test1 values(1,'afas');
-
-DROP sequence seq1;
-CREATE SEQUENCE seq1
-MINVALUE 1 MAXVALUE 100 INCREMENT BY 5 START WITH 50 CYCLE;
-
-select seq1.nextval from dual;
-select seq1.currval from dual;
-
-
-CREATE OR REPLACE TRIGGER test1_bir -- before insert row
-BEFORE INSERT ON test1
-FOR EACH ROW
-WHEN (new.col1 is null)
-BEGIN
-  :new.col1 := seq1.nextval;
-END;
-/
-BEGIN
- FOR i IN 1..14 LOOP
-  INSERT INTO test1 VALUES(null, 'trigger'||to_char(i,'FM09'));
- END LOOP;
- INSERT INTO test1 VALUES(seq1.currval + 1, 'sequence + 1');
- COMMIT;
-END;
-/
-
-SELECT * FROM test1 ORDER BY col2;
-
-DROP TABLE test1;    -- trigger will be dropped too
-DROP sequence seq1;  -- sequence is not bound to the table
-
---set serveroutput on
-
---execute table_print ('T');
-
-
-
-
-
-----------------------------------------------------------------------------------------------
-
-
+--Database objects
+----------------
+--(DBA_OBJECTS)
 --1.
 --Who is the owner of the view DBA_TABLES? Who is the owner of table DUAL? (owner)
 select * from dba_objects where upper(object_name) = 'DBA_TABLES' and object_type = 'VIEW';
@@ -78,7 +31,8 @@ select distinct object_type from dba_objects;
 select owner
 from dba_objects
 group by owner
-having count(distinct object_name) > 10;
+having count(distinct object_type) > 10;
+
 --6.
 --Which users have both triggers and views in the database? (owner)
 select owner
@@ -88,16 +42,16 @@ intersect
 select owner
 from dba_objects
 where object_type = 'VIEW';
+
 --7.
 --Which users have views but don't have triggers? (owner)
-select owner
+select distinct owner
 from dba_objects
 where object_type = 'VIEW'
-MINUS
-select owner
+minus
+select distinct owner
 from dba_objects
 where object_type = 'TRIGGER';
-
 
 --8.
 --Which users have more than 40 tables, but less than 30 indexes? (owner)
@@ -128,6 +82,7 @@ select * from dba_objects where owner = 'NIKOVITS';--DATA_OBJECT_ID  (SEGMENT)
 --SYNONYM,VIEW;
 select distinct object_type from dba_objects
 where data_object_id is NULL;
+
 --PRO
 --SPECIAL TABLE DO NOT HAVE SEGMENTS
 
@@ -144,46 +99,57 @@ intersect
 select distinct object_type from dba_objects
 where data_object_id is not NULL;
 
+--Columns of a table
+------------------
+--(DBA_TAB_COLUMNS)
+
 --13.
 --How many columns nikovits.emp table has? (num)
-
+select count(*) 
+from dba_tab_columns
+where owner = 'NIKOVITS' and table_name = 'EMP';
 
 --14.
 --What is the data type of the 6th column of the table nikovits.emp? (data_type)
-
+select data_type
+from dba_tab_columns
+where owner = 'NIKOVITS' and table_name = 'EMP' and column_id = 6;
 
 --15.
 --Give the owner and name of the tables which have column name beginning with letter 'Z'.
 --(owner, table_name)
-SELECT owner, table_name FROM dba_tab_columns
+select owner, table_name
+from dba_tab_columns
 where column_name like 'Z%';
+
 --16.
 --Give the owner and name of the tables which have at least 8 columns with data type DATE.
 --(owner, table_name)
-SELECT owner, table_name FROM dba_tab_columns
-WHERE data_type='DATE'
-GROUP BY owner, table_name
-HAVING count(*) >= 8;
+select owner, table_name
+from dba_tab_columns
+where data_type = 'DATE'
+group by owner, table_name
+having COUNT(*)>=8;
 
----------------------
-
-create table t1(c1 date, c2 date, c3 date);
-describe t1;
-create table test(c1 date,c2 date,c3 date,c4 date);
-drop table test;
-
-------------------------
 --17.
 --Give the owner and name of the tables whose 1st and 4th column's datatype is VARCHAR2.
 --(owner, table_name)
-SELECT * FROM dba_tab_columns;
+select owner, table_name
+from dba_tab_columns
+where data_type = 'VARCHAR2' and column_id = 1
+intersect
+select owner, table_name
+from dba_tab_columns
+where data_type = 'VARCHAR2' and column_id = 4;
+
 --18.
 --Write a PL/SQL procedure, which prints out the owners and names of the tables beginning with the
 --parameter character string.
 --CREATE OR REPLACE PROCEDURE table_print(p_char VARCHAR2) IS
 CREATE OR REPLACE PROCEDURE table_print(p_char VARCHAR2) IS
 CURSOR curs1 IS
-    select distinct owner, table_name from dba_tables
+    select distinct owner, table_name
+    from dba_tables
     where table_name like upper(p_char)||'%'; --since it is a variable
     rec curs1%ROWTYPE;
 BEGIN
@@ -197,26 +163,51 @@ BEGIN
 END;
 /
 set serveroutput on
-execute table_print('z');
+execute table_print('h');
 
+----------------------------------------------------------------------------------------------
+create table t1(c1 date, c2 date, c3 date);
+describe t1;
+create table test(c1 date,c2 date,c3 date,c4 date);
+drop table t1;
+select * from user_tables;
 
---not ok ?
-select distinct owner, table_name
-from dba_tables
-where table_name like 'V%';
---
+---------------------------TABLE---------------------------------------------
+DROP TABLE test1;
+CREATE TABLE test1(col1 INTEGER PRIMARY KEY, col2 VARCHAR2(20));
 
+select * from user_tables where table_name like 'TE%';
+insert into test1 values(122,'afas');
 
+--------------------------SEQUENCE--------------------------------------------
+DROP sequence seq1;
+CREATE SEQUENCE seq1
+MINVALUE 1 MAXVALUE 100 INCREMENT BY 5 START WITH 50 CYCLE;
 
+select seq1.nextval from dual;
+select seq1.currval from dual;
 
-create view countries_v (tld, cname, region, area, population, gdp, capital, languages)
-as select * from nikovits.orszagok;
+--------------------------TRIGGER-------------------------------------------
+CREATE OR REPLACE TRIGGER test1_bir -- before insert row
+BEFORE INSERT ON test1
+FOR EACH ROW
+WHEN (new.col1 is null)
+BEGIN
+  :new.col1 := seq1.nextval;
+END;
+/
 
-create view rivers_v(region, rname, length, water, area, source_country, end_country, sea, countries)
-as select * from nikovits.folyok;
+BEGIN
+ FOR i IN 1..14 LOOP
+    INSERT INTO test1 VALUES(null, 'trigger'||to_char(i,'FM09'));
+ END LOOP;
+ INSERT INTO test1 VALUES(seq1.currval + 1, 'sequence + 1');
+ COMMIT;
+END;
+/
 
-select * from countries_v;
-select * from rivers_v@aramis;
+SELECT * FROM test1;-- ORDER BY col2;
 
-select *
-from countries_v, rivers_v@aramis;
+DROP TABLE test1;    -- trigger will be dropped too
+DROP sequence seq1;  -- sequence is not bound to the table
+
